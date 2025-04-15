1ca5b427df167ff1a2cb5eb9dbce34b55e58bc6f
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2025-04-15 18:25:21 +0800

    Updates

---
+++ BuildScript.sh
@@ -0,0 +1,15 @@
+project_path=$(cd `dirname $0`; pwd)
+
+project_name="RuntimeViewer"
+
+scheme_name="RuntimeViewerUsingAppKit"
+
+development_mode="Release"
+
+build_path=${project_path}/build
+
+xcodebuild build -scheme ${scheme_name} -configuration ${development_mode} -destination 'generic/platform=macOS' CONFIGURATION_BUILD_DIR=${build_path} ARCHS="x86_64 arm64e"
+
+exit 0
+
+
