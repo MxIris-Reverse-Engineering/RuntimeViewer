be39a2dc7c8a6e218c170ffc894ce7dde7c9bfcd
Mx-Iris <mxiris@126.com>
2025-11-12 00:04:37 +0800

    Improve basic functionality

--- RuntimeViewerPackages/Sources/RuntimeViewerApplication/AppServices.swift
+++ RuntimeViewerPackages/Sources/RuntimeViewerApplication/AppServices.swift
@@ -4,8 +4,6 @@ import RuntimeViewerArchitectures
 
 public final class AppServices: NSObject {
 //    public static let shared = AppServices()
-
-    public let runtimeEngineManager = RuntimeEngineManager()
     
     @Observed
     public var runtimeEngine: RuntimeEngine = .shared
