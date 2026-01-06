2973772574e7ce8db54fdf1d4387939c8982a6cd
Mx-Iris <mxiris@126.com>
2026-01-07 02:08:07 +0800

    Update Package.swift and workspace

--- RuntimeViewerUsingAppKit/RuntimeViewerUsingAppKit/Main/MainSplitViewController.swift
+++ RuntimeViewerUsingAppKit/RuntimeViewerUsingAppKit/Main/MainSplitViewController.swift
@@ -19,27 +19,46 @@ final class MainSplitViewController: NSSplitViewController {
         splitView.identifier = .makeIdentifier(of: Self.self)
     }
 
+    private static let sidebarMinimumWidth: CGFloat = 300
+
+    private static let contentMinimumWidth: CGFloat = 300
+
+    private static let inspectorMinimumWidth: CGFloat = 260
+
     func setupSplitViewItems() {
         splitViewItems[safe: 0]?.do {
-            $0.minimumThickness = 300
+            $0.minimumThickness = Self.sidebarMinimumWidth
             $0.maximumThickness = NSSplitViewItem.unspecifiedDimension
             $0.holdingPriority = .init(261)
         }
 
         splitViewItems[safe: 1]?.do {
-            $0.minimumThickness = 300
+            $0.minimumThickness = Self.contentMinimumWidth
             $0.maximumThickness = NSSplitViewItem.unspecifiedDimension
             $0.holdingPriority = .init(250)
         }
 
         splitViewItems[safe: 2]?.do {
-            $0.minimumThickness = 260
+            $0.minimumThickness = Self.inspectorMinimumWidth
             $0.maximumThickness = NSSplitViewItem.unspecifiedDimension
             $0.holdingPriority = .init(261)
         }
 
+        let autosaveName = "com.JH.RuntimeViewer.\(Self.self).autosaveName"
+
+        let fullAutosaveName = "NSSplitView Subview Frames \(autosaveName)"
+
+        let isInitialSetupAutosaveName = UserDefaults.standard.array(forKey: fullAutosaveName) == nil
+
         splitView.autosaveName = nil
-        splitView.autosaveName = "com.JH.RuntimeViewer.\(Self.self).autosaveName"
+        splitView.autosaveName = autosaveName
+
+        DispatchQueue.main.async { [self] in
+            if isInitialSetupAutosaveName {
+                splitView.setPosition(Self.sidebarMinimumWidth, ofDividerAt: 0)
+                splitView.setPosition(view.bounds.width - Self.inspectorMinimumWidth, ofDividerAt: 1)
+            }
+        }
     }
 
     func setupBindings(for viewModel: MainViewModel) {
