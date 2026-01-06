2118a40aaee55c0d01bccc5fea66a33feae87d98
Mx-Iris <mxiris@126.com>
2026-01-07 02:08:07 +0800

    Update Package.swift and workspace

--- ArchiveScript.sh
+++ ArchiveScript.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
 project_path=$(cd `dirname $0`; pwd)
 
 project_name="RuntimeViewer"
@@ -6,23 +11,44 @@ scheme_name="RuntimeViewer macOS"
 
 development_mode="Release"
 
-build_path=${project_path}/build
+build_path=${project_path}/archive
 
-exportOptionsPlistPath=${project_path}/ArchiveExportConfig.plist
+export_options_plist_path=${project_path}/ArchiveExportConfig.plist
 
-exportAppPath=${project_path}/Archives
+catalyst_helper_export_path=${project_path}/RuntimeViewerUsingAppKit
 
+catalyst_helper_app_path=${catalyst_helper_export_path}/RuntimeViewerCatalystHelper.app
+
+catalyst_helper_archive_path=${build_path}/RuntimeViewerCatalystHelper.xcarchive
 
 echo '///-----------'
-echo '/// Building RuntimeViewer CatalystHelper: '${development_mode}
+echo '/// Archiving RuntimeViewer CatalystHelper: '${development_mode}
 echo '///-----------'
 
 xcodebuild \
-build \
+archive \
 -workspace ${project_path}/${project_name}.xcworkspace \
 -scheme 'RuntimeViewer CatalystHelper' \
 -configuration ${development_mode} \
--destination 'generic/platform=macOS,variant=Mac Catalyst' || exit
+-destination 'generic/platform=macOS,variant=Mac Catalyst' \
+-archivePath ${catalyst_helper_archive_path} | xcbeautify || exit
+
+if [ -d "${catalyst_helper_app_path}" ]; then
+    rm -rf "${catalyst_helper_app_path}"
+fi
+
+xcodebuild \
+-exportArchive -archivePath ${catalyst_helper_archive_path} \
+-configuration ${development_mode} \
+-exportPath ${catalyst_helper_export_path} \
+-exportOptionsPlist ${export_options_plist_path} \
+-quiet | xcbeautify || exit
+
+echo '///------------'
+echo '/// RuntimeViewer CatalystHelper Archive Complete  '
+echo '///-----------='
+echo ''
+
 
 echo '///-----------'
 echo '/// Archiving '${scheme_name}': '${development_mode}
@@ -31,7 +57,7 @@ echo '///-----------'
 xcodebuild \
 archive \
 -workspace ${project_path}/${project_name}.xcworkspace \
--scheme ${scheme_name} \
+-scheme "${scheme_name}" \
 -configuration ${development_mode} \
 -destination 'generic/platform=macOS' \
 -archivePath ${build_path}/${project_name}.xcarchive || exit
@@ -39,10 +65,8 @@ archive \
 open ${build_path}/${project_name}.xcarchive
 
 echo '///------------'
-echo '/// App Archive Complete  '
+echo '/// RuntimeViewer macOS Archive Complete  '
 echo '///-----------='
 echo ''
 
 exit 0
-
-
