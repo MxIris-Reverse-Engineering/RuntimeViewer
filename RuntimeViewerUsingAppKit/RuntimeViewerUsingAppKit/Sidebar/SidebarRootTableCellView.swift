fe2c2d8d6d57bc64726d6d545acff3c404253424
Mx-Iris <mxiris@126.com>
2026-03-01 15:28:15 +0800

    improve: add generation option UIs, Swift member address, and sidebar delegate
    Add UI options for ObjC property accessor/method IMP address comments and
    Swift member address printing. Simplify SidebarRootTableCellView background
    style handling. Wire up OutlineViewDelegate alongside existing DataSource.
    Signed-off-by: Mx-Iris <mxiris@126.com>

--- RuntimeViewerUsingAppKit/RuntimeViewerUsingAppKit/Sidebar/SidebarRootTableCellView.swift
+++ RuntimeViewerUsingAppKit/RuntimeViewerUsingAppKit/Sidebar/SidebarRootTableCellView.swift
@@ -4,25 +4,10 @@ import RuntimeViewerArchitectures
 import RuntimeViewerApplication
 
 final class SidebarRootTableCellView: ImageTextTableCellView {
-    override var backgroundStyle: NSView.BackgroundStyle {
-        didSet {
-            switch backgroundStyle {
-            case .normal:
-                _imageView.symbolConfiguration = .init(hierarchicalColor: .controlAccentColor)
-            case .emphasized:
-                _imageView.symbolConfiguration = .preferringMonochrome()
-            case .raised:
-                _imageView.symbolConfiguration = .preferringMonochrome()
-            case .lowered:
-                _imageView.symbolConfiguration = .preferringMonochrome()
-            @unknown default:
-                break
-            }
-        }
-    }
-
     override func setup() {
         super.setup()
+        
+        _imageView.contentTintColor = .controlAccentColor
     }
 
     func bind(to viewModel: SidebarRootCellViewModel) {
