927baa5e6e495ee6ea0af5828aa92587fb74bc45
Mx-Iris <mxiris@126.com>
2026-05-23 23:36:04 +0800

    docs(xpc-connection): note why peer.activate() must trail the modifier

--- RuntimeViewerPackages/Package.swift
+++ RuntimeViewerPackages/Package.swift
@@ -309,10 +309,6 @@ let package = Package(
             )
         ),
         .package(
-            url: "https://github.com/MxIris-macOS-Library-Forks/SwiftyXPC",
-            from: "0.5.100"
-        ),
-        .package(
             local: .package(
                 path: MxIrisStudioWorkspace.personalLibraryMacOSDirectory.libraryPath("swift-helper-service"),
                 isRelative: true,
@@ -467,7 +463,6 @@ let package = Package(
             name: "RuntimeViewerService",
             dependencies: [
                 .product(name: "RuntimeViewerCommunication", package: "RuntimeViewerCore"),
-                .product(name: "SwiftyXPC", package: "SwiftyXPC", condition: .when(platforms: appkitPlatforms)),
                 .product(name: "MachInjector", package: "MachInjector", condition: .when(platforms: appkitPlatforms)),
                 .product(name: "HelperCommunication", package: "swift-helper-service", condition: .when(platforms: appkitPlatforms)),
                 .product(name: "HelperService", package: "swift-helper-service", condition: .when(platforms: appkitPlatforms)),
@@ -484,7 +479,6 @@ let package = Package(
             dependencies: [
                 "RuntimeViewerServiceHelper",
                 .product(name: "RuntimeViewerCommunication", package: "RuntimeViewerCore"),
-                .product(name: "SwiftyXPC", package: "SwiftyXPC", condition: .when(platforms: appkitPlatforms)),
                 .product(name: "Dependencies", package: "swift-dependencies"),
                 .product(name: "HelperCommunication", package: "swift-helper-service", condition: .when(platforms: appkitPlatforms)),
                 .product(name: "HelperClient", package: "swift-helper-service", condition: .when(platforms: appkitPlatforms)),
